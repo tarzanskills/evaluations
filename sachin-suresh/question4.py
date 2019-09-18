51473cd02388bcf03080f512d4fb74fdd8c718df
sachinsuresh <sachinsuresh00@gmail.com>
2019-09-18 08:58:46 +0530

    Latest update

--- sachin-suresh/question4.py
+++ sachin-suresh/question4.py
@@ -1,4 +1,6 @@
+import os
 from os import path
+from ast import literal_eval
 
 def register():
     print("Welcome to user registration")
@@ -27,10 +29,14 @@ def login():
 def display_cart(username):
     shopping_cart = open('/home/sachin/tarzan/evaluations/sachin-suresh/' + username + '/shopping_cart.txt', 'r')
     item_list = shopping_cart.read().split()
+    print("Your shopping cart has")
     for i in item_list:
-        item_name = i.keys()
-        item_num = i.values()
-        
+        item_dict = literal_eval("{'a': 1}")
+        item_name = item_dict.items(0)
+        item_num = item_dict.items(1)
+        print(item_name,item_num)
+    shopping_cart.close()
+
 def add_to_cart(username):
     dictionary = {}
     case = 'y'
@@ -45,70 +51,70 @@ def add_to_cart(username):
             continue
         else:
             break
-    display_cart(username)
     shopping_cart.close()
-#
-# def remove_from_cart(username):
-#     item_name = input("Enter the item you want to remove from the cart: ")
-#     shopping_cart = open('/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart.txt', 'a+')
-#     shopping_cart_new = open('/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart_new.txt', 'a+')
-#     shopping_cart.seek(0,0)
-#     while (1):
-#         item = shopping_cart.read()
-#         if item == '':
-#             break
-#         item_list = item.split()
-#         if item_list[0] != item_name:
-#             shopping_cart_new.write(item)
-#     shopping_cart.close()
-#     shopping_cart_new.close()
-#     os.remove('/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart.txt')
-#     os.rename('/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart_new.txt','/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart.txt')
-#
-# def edit_quantity(username):
-#     item_name = input("Enter the item you want to edit in quantity: ")
-#     item_num = int(input("Enter the new quantity of item: "))
-#     shopping_cart = open('/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart.txt', 'a+')
-#     shopping_cart_new = open('/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart_new.txt', 'a+')
-#     shopping_cart.seek(0, 0)
-#     while (1):
-#         item = shopping_cart.read()
-#         if item == '':
-#             break
-#         item_list = item.split()
-#         if item_list[0] != item_name:
-#             shopping_cart_new.write(item)
-#         else:
-#             shopping_cart_new.write(str([username,item_name,item_num]))
-#     shopping_cart.close()
-#     shopping_cart_new.close()
-#     os.remove('/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart.txt')
-#     os.rename('/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart_new.txt','/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart.txt')
-#
-# def logout():
-#     print("You are logged out")
+    display_cart(username)
 
-# def delete_account(username):
-#     check = input("Are you sure you want to delete all the data from your account? Press y to continue: ")
-#     if check == 'y':
-#         registry_file = open('/home/sachin/tarzan/evaluations/sachin-suresh/reg_file.txt', 'r')
-#         registry_file_new = open('/home/sachin/tarzan/evaluations/sachin-suresh/reg_file_new.txt', 'r')
-#         while (1):
-#             registry = registry_file.read()
-#             if registry == '':
-#                 break
-#             registry_list = registry.split()
-#             if username not in registry_list:
-#                 registry_file_new.write(str([username,))
-#         registry_file.close()
-#         registry_file_new.close()
-#         os.remove('/home/sachin/tarzan/evaluations/sachin-suresh/reg_file.txt')
-#         os.rename('/home/sachin/tarzan/evaluations/sachin-suresh/reg_file_new.txt.txt',
-#                   '/home/sachin/tarzan/evaluations/sachin-suresh/reg_file.txt')
-#     else:
-#         print("Delete operation aborted!")
+def remove_from_cart(username):
+    item_name = input("Enter the item you want to remove from the cart: ")
+    shopping_cart = open('/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart.txt', 'a+')
+    shopping_cart_new = open('/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart_new.txt', 'a+')
+    shopping_cart.seek(0,0)
+    while (1):
+        item = shopping_cart.read()
+        if item == '':
+            break
+        item_list = item.split()
+        if item_list[0] != item_name:
+            shopping_cart_new.write(item)
+    shopping_cart.close()
+    shopping_cart_new.close()
+    os.remove('/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart.txt')
+    os.rename('/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart_new.txt','/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart.txt')
 
-logged = True
+def edit_quantity(username):
+    item_name = input("Enter the item you want to edit in quantity: ")
+    item_num = int(input("Enter the new quantity of item: "))
+    shopping_cart = open('/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart.txt', 'a+')
+    shopping_cart_new = open('/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart_new.txt', 'a+')
+    shopping_cart.seek(0, 0)
+    while (1):
+        item = shopping_cart.read()
+        if item == '':
+            break
+        item_list = item.split()
+        if item_list[0] != item_name:
+            shopping_cart_new.write(item)
+        else:
+            shopping_cart_new.write(str([username,item_name,item_num]))
+    shopping_cart.close()
+    shopping_cart_new.close()
+    os.remove('/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart.txt')
+    os.rename('/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart_new.txt','/home/sachin/tarzan/evaluations/sachin-suresh/shopping_cart.txt')
+
+def logout():
+    print("You are logged out")
+
+def delete_account(username):
+    check = input("Are you sure you want to delete all the data from your account? Press y to continue: ")
+    if check == 'y':
+        registry_file = open('/home/sachin/tarzan/evaluations/sachin-suresh/reg_file.txt', 'r')
+        registry_file_new = open('/home/sachin/tarzan/evaluations/sachin-suresh/reg_file_new.txt', 'r')
+        while (1):
+            registry = registry_file.read()
+            if registry == '':
+                break
+            registry_list = registry.split()
+            if username not in registry_list:
+                registry_file_new.write(str([username,))
+        registry_file.close()
+        registry_file_new.close()
+        os.remove('/home/sachin/tarzan/evaluations/sachin-suresh/reg_file.txt')
+        os.rename('/home/sachin/tarzan/evaluations/sachin-suresh/reg_file_new.txt.txt',
+                  '/home/sachin/tarzan/evaluations/sachin-suresh/reg_file.txt')
+    else:
+        print("Delete operation aborted!")
+
+logged = True #False
 username = 'default'
 print("Shopping App")
 user_input = input("Enter your choice:\n1.Register\n2. Login\n3. Add to cart\n4. Remove from Cart\n5. Edit quantity of items\n6. Logout\n7.Delete Account")
@@ -122,7 +128,7 @@ elif user_input == '2':
         print("Invalid credentials.")
 elif user_input == '3':
     if logged == True:
-        add_to_cart('sachin')
+        add_to_cart('sachin') #username
     else:
         print("User not logged in. Please login")
 elif user_input == '4':
